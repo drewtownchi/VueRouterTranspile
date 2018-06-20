d600ef788dee63615c42aaeafd93daa763a57ff4
Andrew Town <atown@group1.local>
2018-06-20 13:55:35 -0500

    Add dynamic route and transpile

--- src/router.js
+++ src/router.js
@@ -1,13 +1,13 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
-import About from "./views/About.vue";
+const About = () =>
+  import ("./views/About.vue");
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: "/",
       name: "home",
       component: Home
